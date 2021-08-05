f7e7f8611b8007bf153215cc322c280aa8b13d6a
pessoasnil <Metroide171190y@>
2021-08-04 22:03:33 -0300

    Implementando Sentry

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,9 +4,6 @@ from django.http import HttpResponse
 # from django.shortcuts import render
 
 
-# Create your views here.
-
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>',)
-
-
+    # raise ValueError()
+    return HttpResponse('<html><body>Olá Django</body></html>', )
