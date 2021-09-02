ee3d02a87a29eda7dff5e7871cbd9898413bb5ea
pessoasnil <Metroide171190y@>
2021-09-01 23:45:53 -0300

    deplou 6

--- pypro/settings.py
+++ pypro/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+from decouple import Csv
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 AUTH_USER_MODEL = 'base.User'
 
 # Application definition
